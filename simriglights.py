e461560480b05b076aeddac3e417c398fecccf6e
Kyler Freas <kmf5285@rit.edu>
2021-03-18 19:17:01 -0400

    added main script, ready for integration testing

---
+++ simriglights.py
@@ -0,0 +1,24 @@
+from colour import Color
+from e131.wled import Wled
+from display.rpmgauge import RpmGauge
+from raceparse.iracingstream import IracingStream
+
+def main():
+    data_stream = IracingStream.get_stream()
+    latest = data_stream.latest()
+
+    #TODO: Set externally
+    controller = Wled.connect('192.168.1.65', 1)
+
+    # Grab redline RPM for current car
+    rpm_strip = RpmGauge(50, Color('green'), Color('red'), latest['redline'])
+
+    while data_stream.is_active():
+        latest = data_stream.latest()
+        rpm_strip.set_rpm(latest['rpm'])
+        controller.update(rpm_strip.to_color_list())
+
+    data_stream.stop()
+
+if __name__ == '__main__':
+    main()
