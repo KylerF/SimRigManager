d63ccb3fe249cffd1a7745f9fe5b3f3a490b08e5
Kyler Freas <kmf5285@rit.edu>
2021-03-19 10:57:18 -0400

    comments and cleanup

--- e131/wled.py
+++ e131/wled.py
@@ -1,9 +1,19 @@
+"""
+WLED controller class to handle ACN communications and update
+the color of the LEDs
+"""
 import sacn
 import math
 
 class Wled:
+    """
+    Used to connect to the WLED controller
+    """
     @staticmethod
     def connect(ip, universe):
+        """
+        Create and return a controller ready for use
+        """
         wled = Wled()
         wled.ip = ip
         wled.universe = universe
@@ -12,18 +22,27 @@ class Wled:
         return wled
 
     def stop(self):
+        """
+        Close the communication and destroy the sender (recreated on next connection)
+        """
         if self.sender:
             self.sender.deactivate_output(1)
             self.sender.stop()
             self.sender = None
 
     def reconnect(self):
+        """
+        Recreate and set up the sender
+        """
         self.sender = sacn.sACNsender()
         self.sender.start()
         self.sender.activate_output(self.universe)
         self.sender[self.universe].destination = self.ip
 
     def update(self, color_list):
+        """
+        Send some colors down stream
+        """
         data = []
         
         for color in color_list:
--- tests/iracingstream_test.py
+++ tests/iracingstream_test.py
@@ -1,23 +1,36 @@
+"""
+Unit tests for the IracingStream module
+Test data is stored in .bin files located in the /data directory
+Additional files can be generating using the irsdk script
+(https://github.com/kutu/pyirsdk/blob/master/tutorials/02%20Using%20irsdk%20script.md)
+TODO: Close the files that irsdk is leaving open even after stopping the stream
+"""
 import unittest
 from raceparse.iracingstream import IracingStream
 
 class TestIracingStream(unittest.TestCase):
     def test_empty_stream(self):
+        """
+        Test creating a stream without a running iRacing instance and no test file
+        """
         iracing_stream = IracingStream.get_stream()
         iracing_stream.update()
 
         self.assertFalse(iracing_stream.is_active, 
-        msg='Stream should not think it is active when there is no data')
+        msg="Stream should not think it is active when there is no data")
         
         self.assertEqual(iracing_stream.latest(), {}, 
-        msg='Stream should not have data when there is no data!')
+        msg="Stream should not have data when there is no data!")
 
         iracing_stream.stop()
         self.assertIsNone(iracing_stream.ir, 
-        msg='IRSDK object should have been destroyed upon closing stream')
+        msg="IRSDK object should have been destroyed upon closing stream")
 
     def test_stationary_stream(self):
-        iracing_stream = IracingStream.get_stream(test_file='tests/data/ir01_idle_watkins.bin')
+        """
+        Test getting data while the car is idling
+        """
+        iracing_stream = IracingStream.get_stream(test_file="tests/data/ir01_idle_watkins.bin")
 
         self.assertTrue(iracing_stream.is_active, 
         msg="Stream should be active on test file")
@@ -25,13 +38,18 @@ class TestIracingStream(unittest.TestCase):
         iracing_stream.update()
         snapshot = iracing_stream.latest()
 
-        self.assertEqual(snapshot['rpm'], snapshot['idle_rpm'], 
+        self.assertEqual(snapshot["rpm"], snapshot["idle_rpm"], 
         msg="Car should be at idling")
 
         iracing_stream.stop()
+        self.assertIsNone(iracing_stream.ir, 
+        msg="IRSDK object should have been destroyed upon closing stream")
     
     def test_stationary_rev_stream(self):
-        iracing_stream = IracingStream.get_stream(test_file='tests/data/ir01_revlimiter_watkins.bin')
+        """
+        Test getting data while the car is bouncing off the rev limiter
+        """
+        iracing_stream = IracingStream.get_stream(test_file="tests/data/ir01_revlimiter_watkins.bin")
 
         self.assertTrue(iracing_stream.is_active, 
         msg="Stream should be active on test file")
@@ -39,13 +57,18 @@ class TestIracingStream(unittest.TestCase):
         iracing_stream.update()
         snapshot = iracing_stream.latest()
 
-        self.assertTrue(snapshot['redline']-100 <= snapshot['rpm'] <= snapshot['redline'], 
+        self.assertTrue(snapshot["redline"]-100 <= snapshot["rpm"] <= snapshot["redline"], 
         msg="Car should be within 100 RPM of redline (bouncing off rev limiter)")
 
         iracing_stream.stop()
+        self.assertIsNone(iracing_stream.ir, 
+        msg="IRSDK object should have been destroyed upon closing stream")
 
     def test_full_speed_stream(self):
-        iracing_stream = IracingStream.get_stream(test_file='tests/data/ir01_fullspeed_watkins.bin')
+        """
+        Test getting data while the car is at speed
+        """
+        iracing_stream = IracingStream.get_stream(test_file="tests/data/ir01_fullspeed_watkins.bin")
 
         self.assertTrue(iracing_stream.is_active, 
         msg="Stream should be active on test file")
@@ -53,10 +76,12 @@ class TestIracingStream(unittest.TestCase):
         iracing_stream.update()
         snapshot = iracing_stream.latest()
 
-        self.assertEqual(snapshot['speed'], 198,  
+        self.assertEqual(snapshot["speed"], 198,  
         msg="Speed should be 198 MPH")
 
         iracing_stream.stop()
+        self.assertIsNone(iracing_stream.ir, 
+        msg="IRSDK object should have been destroyed upon closing stream")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
