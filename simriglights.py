e8da7ae0446a214bbd1ad7c9c8e7be25983a20a8
Kyler Freas <kmf5285@rit.edu>
2021-03-18 22:22:45 -0400

    properly clean up resources after keyboard interrupt. drop connection to wled when there is no data stream active.

--- simriglights.py
+++ simriglights.py
@@ -1,7 +1,7 @@
-from colour import Color
-from e131.wled import Wled
-from display.rpmgauge import RpmGauge
 from raceparse.iracingstream import IracingStream
+from display.rpmgauge import RpmGauge
+from e131.wled import Wled
+from colour import Color
 
 def main():
     data_stream = IracingStream.get_stream()
@@ -10,19 +10,26 @@ def main():
     controller = Wled.connect('192.168.1.65', 1)
 
     # Grab redline RPM for current car
-    rpm_strip = RpmGauge(50, Color('green'), Color('red'))
+    rpm_strip = RpmGauge(50, Color('red'), Color('green'))
 
-    while data_stream.is_active():
-        latest = data_stream.latest()
+    try:
+        while True:
+            latest = data_stream.latest()
 
-        if rpm_strip.redline != latest['redline']:
-            rpm_strip.set_redline(latest['redline'])
+            if not data_stream.is_active:
+                controller.stop()
+                continue
+            else:
+                controller.reconnect()
 
-        rpm_strip.set_rpm(latest['rpm'])
+            if rpm_strip.redline != latest['redline']:
+                rpm_strip.set_redline(latest['redline'])
 
-        controller.update(rpm_strip.to_color_list())
-
-    data_stream.stop()
+            rpm_strip.set_rpm(latest['rpm'])
+            controller.update(rpm_strip.to_color_list())
+    except KeyboardInterrupt:
+        data_stream.stop()
+        controller.stop()
 
 if __name__ == '__main__':
     main()
