d081d712e71b42df5f2fbded756566670de20800
Kyler Freas <kmf5285@rit.edu>
2021-03-25 09:06:23 -0400

    added starter methods to matrix

--- display/squarematrix.py
+++ display/squarematrix.py
@@ -1,9 +1,24 @@
+from matrixtextmap import numbers16
 from colour import Color
 
 class SquareMatrix:
-    def __init__(self, width, panes):
+    def __init__(self, width, color):
         self.width = width
-        self.panes = panes
+        self.led_count = width*width
+        self.color = color
 
-    def get_panes(self):
-        return self.panes
+    def set_text(self, text):
+        self.text = str(text)
+
+    def to_color_list(self):
+        color_list = []
+
+        for index in range(0, self.led_count):
+            if index in numbers16[self.text]:
+                color_list.append(self.color.rgb[0])
+                color_list.append(self.color.rgb[1])
+                color_list.append(self.color.rgb[2])
+            else:
+                color_list.extend([0]*3)
+
+        return color_list
