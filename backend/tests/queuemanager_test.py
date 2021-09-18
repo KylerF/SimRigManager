82c4e8d56c9e418450903b348a69a060dc975edb
Kyler Freas <kmf5285@rit.edu>
2021-09-18 12:23:53 -0400

    add exception messages

--- backend/tests/queuemanager_test.py
+++ backend/tests/queuemanager_test.py
@@ -20,8 +20,10 @@ class TestQueueManager(unittest.TestCase):
         """
         Test the initial state of the queue manager
         """
-        self.assertEqual(self.queue_manager.channels, {})
-        self.assertEqual(self.queue_manager.subscribers, {})
+        self.assertEqual(self.queue_manager.channels, {}, 
+            "Expected empty channel list after init")
+        self.assertEqual(self.queue_manager.subscribers, {}, 
+            "Expected empty subscriber list after init")
 
     def test_open_channel(self):
         """
@@ -29,7 +31,8 @@ class TestQueueManager(unittest.TestCase):
         """
         self.queue_manager.open_channel("test")
 
-        self.assertIsInstance(self.queue_manager.channels["test"], Queue)
+        self.assertIsInstance(self.queue_manager.channels["test"], Queue, 
+            "Expected a new queue to be created")
 
     def test_put_get(self):
         """
@@ -37,20 +40,24 @@ class TestQueueManager(unittest.TestCase):
         """
         self.queue_manager.open_channel("test")
 
-        self.assertTrue(self.queue_manager.put("test", "test"))
-        self.assertEqual(self.queue_manager.get("test"), "test")
+        self.assertTrue(self.queue_manager.put("test", "test"), 
+            "Failed to put message on the queue")
+        self.assertEqual(self.queue_manager.get("test"), "test", 
+            "Failed to retrieve message from the queue")
 
     def test_put_invalid_channel(self):
         """
         Test attempting to put data in a non-existent queue
         """
-        self.assertFalse(self.queue_manager.put("test", "test"))
+        self.assertFalse(self.queue_manager.put("test", "test"), 
+            "Should get false return value when pushing to an invalid channel")
 
     def test_get_invalid_channel(self):
         """
         Test attempting to put data in a non-existent queue
         """
-        self.assertIsNone(self.queue_manager.get("test"))
+        self.assertIsNone(self.queue_manager.get("test"), 
+            "Invalid channel query should return None")
 
     def test_get_empty(self):
         """
@@ -58,7 +65,8 @@ class TestQueueManager(unittest.TestCase):
         """
         self.queue_manager.open_channel("test")
 
-        self.assertIsNone(self.queue_manager.get("test"))
+        self.assertIsNone(self.queue_manager.get("test"), 
+            "Getting from an empty channel should return None")
 
     def test_subscribe(self):
         """
@@ -67,11 +75,13 @@ class TestQueueManager(unittest.TestCase):
         self.queue_manager.open_channel("test")
         self.queue_manager.subscribe("test", self.__subscribe_callback)
 
-        self.assertEqual(len(self.queue_manager.subscribers["test"]), 1)
+        self.assertEqual(len(self.queue_manager.subscribers["test"]), 1, 
+            "Subscriber was not added")
 
         self.queue_manager.put("test", "test")
 
-        self.assertEqual(self.data_sink, "test")
+        self.assertEqual(self.data_sink, "test", 
+            "Subscriber callback function was not called successfully")
 
     def test_close_channel(self):
         """
@@ -81,13 +91,15 @@ class TestQueueManager(unittest.TestCase):
         self.queue_manager.put("test", "test")
         self.queue_manager.close_channel("test")
 
-        self.assertEqual(self.queue_manager.channels, {})
+        self.assertEqual(self.queue_manager.channels, {}, 
+            "Should have removed the channel")
 
     def test_close_invalid_channel(self):
         """
         Test attempting to put data in a non-existent queue
         """
-        self.assertFalse(self.queue_manager.close_channel("test"))
+        self.assertFalse(self.queue_manager.close_channel("test"), 
+            "Expected false return value when closing an invalid channel")
 
     def test_close_all_channels(self):
         """
@@ -98,7 +110,8 @@ class TestQueueManager(unittest.TestCase):
         self.queue_manager.put("test1", "test")
         self.queue_manager.close_all()
 
-        self.assertEqual(self.queue_manager.channels, {})
+        self.assertEqual(self.queue_manager.channels, {}, 
+            "Expected empty channel list after closing all channels")
 
 
     def __subscribe_callback(self, data):
@@ -106,3 +119,7 @@ class TestQueueManager(unittest.TestCase):
         Callback function for testing subscriptions
         """
         self.data_sink = data
+
+
+if __name__ == "__main__":
+    unittest.main()
