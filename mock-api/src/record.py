b661a3a145d5506a352bc7e142e6a621e26a2e3a
Kyler Freas <kmf5285@rit.edu>
2021-11-17 22:42:17 -0500

    save data in memory, dump to file after recording is finished

--- mock-api/src/record.py
+++ mock-api/src/record.py
@@ -1,12 +1,15 @@
 import websocket
 import json
+import atexit
+
+iracing_data = []
+
+def save_data():
+    with open("./data/test.json",'w') as file:
+        json.dump(iracing_data, file, indent = 4)
 
 def on_message(ws, message):
-    with open("./data/jefferson.json",'r+') as file:
-        file_data = json.load(file)
-        file_data.append(json.loads(message))
-        file.seek(0)
-        json.dump(file_data, file, indent = 4)
+    iracing_data.append(json.loads(message))
 
 def on_error(ws, error):
     print(error)
@@ -15,6 +18,7 @@ def on_close(ws, close_status_code, close_msg):
     print("### closed ###")
 
 if __name__ == "__main__":
+    atexit.register(save_data)
     websocket.enableTrace(True)
     ws = websocket.WebSocketApp("ws://192.168.1.201:8000/stream",
                               on_message=on_message,
