972f32293bf4be214cf012507c47b90b4f59dc14
Kyler Freas <kmf5285@rit.edu>
2021-11-21 01:39:00 -0500

    load data into memory and dump to file

--- mock-api/src/record.py
+++ mock-api/src/record.py
@@ -1,11 +1,19 @@
+from os import path, getcwd
 import websocket
-import json
+import argparse
 import atexit
+import json
+
 
 iracing_data = []
+output_file = ""
 
 def save_data():
-    with open("./data/test.json",'w') as file:
+    if not iracing_data:
+        return
+
+    print("Saving data to " + output_file)
+    with open(output_file,"w") as file:
         json.dump(iracing_data, file, indent = 4)
 
 def on_message(ws, message):
@@ -18,11 +26,32 @@ def on_close(ws, close_status_code, close_msg):
     print("### closed ###")
 
 if __name__ == "__main__":
-    atexit.register(save_data)
+    parser = argparse.ArgumentParser(description="iRacing data recorder")
+    parser.add_argument("file", help="output file name")
+    parser.add_argument(
+        "--host", 
+        default="localhost", 
+        help="hostname of iracing server"
+    )
+    parser.add_argument(
+        "--port", 
+        default=8000, 
+        help="port of iracing server"
+    )
+    args = parser.parse_args()
+
+    current_path = path.abspath(getcwd())
+    output_file = path.join(
+        current_path, "data",
+        f"{args.file}.json"
+    )
+
     websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("ws://192.168.1.201:8000/stream",
-                              on_message=on_message,
-                              on_error=on_error,
-                              on_close=on_close)
+    ws = websocket.WebSocketApp(f"ws://{args.host}:{args.port}/stream",
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
+
+    atexit.register(save_data)
 
     ws.run_forever()
