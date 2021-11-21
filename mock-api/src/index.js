b133c16d77509e024689bc76119a150ba4a574fc
Kyler Freas <kmf5285@rit.edu>
2021-11-21 13:22:16 -0500

    made stream the same for all connections

--- mock-api/src/index.js
+++ mock-api/src/index.js
@@ -1,12 +1,38 @@
+/**
+ * Simulate a real-time stream of iRacing data.
+ * The stream begins immediately so that all connnections will 
+ * receive the same data at any given time.
+ * 
+ * To create a JSON file with test data, run record.py and point it
+ * to your SimRigAPI instance with an iRacing session running. This will
+ * record all session data to a JSON file in ./data.
+ * 
+ * Endpoints:
+ *  ws://{baseURL}/stream: returns a JSON stream of iRacing data
+ *  http://{baseURL}/latest: returns the latest iRacing data
+ */
+
 const StreamArray = require('stream-json/streamers/StreamArray');
 const expressWebSocket = require('express-ws');
 const express = require('express');
 const fs = require('fs');
 
+let currentFrame = {};
+
 const jsonStream = StreamArray.withParser();
 const app = express();
 
-let currentFrame = {};
+const stream = fs.createReadStream('./src/data/test.json').pipe(jsonStream.input);
+
+jsonStream.on('data', ({key, value}) => {
+  currentFrame = value;
+  slowDownStream(stream);
+});
+
+jsonStream.on('end', () => {
+  console.log('Stream ended');
+});
+
 
 app.get("/latest", (req, res) => {
   res.send(currentFrame);
@@ -19,15 +45,8 @@ expressWebSocket(app, null, {
 });
 
 app.ws('/stream', (ws, req) => {
-  const stream = fs.createReadStream('./src/data/daytona.json').pipe(jsonStream.input);
-
-  jsonStream.on('data', ({key, value}) => {
-    currentFrame = value;
-    writeSessionData(ws, stream);
-  });
-
-  jsonStream.on('end', ({key, value}) => {
-    console.log('End of file');
+  jsonStream.on('data', () => {
+    ws.send(JSON.stringify(currentFrame));
   });
 
   ws.on('message', msg => {
@@ -42,17 +61,20 @@ app.ws('/stream', (ws, req) => {
 app.listen(8002);
 
 /**
- * Function to write session data to console, then sleep for a 30 ms
+ * Slows down a JSON file stream to simulate a real-time stream
+ * 
+ * @param {*} stream readstream to be paused
  */
-async function writeSessionData(ws, stream) {
+async function slowDownStream(stream) {
   stream.pause();
-  ws.send(JSON.stringify(currentFrame));
   await sleep(20);
   stream.resume();
 }
 
 /**
- * Function to sleep for a given time
+ * Blocking function to sleep for a given time
+ * 
+ * @param {int} ms time in milliseconds
  */
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
