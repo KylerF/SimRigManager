d81a18df6e7bfb3c2fba6113647db1a1ed89dec6
Kyler Freas <kmf5285@rit.edu>
2022-10-03 05:53:45 -0400

    updated to support multiple universes

--- backend/e131/wled.py
+++ backend/e131/wled.py
@@ -1,32 +1,43 @@
 import sacn
 import math
 
+from e131.sender_info import source
+from e131.exceptions import WledMaxPixelsExceeded
+
 class Wled:
     """
     WLED controller object to handle ACN communications and update
     the color of the connected LEDs
     """
     is_connected = False
+    universes = 0
 
 
     @staticmethod
-    def connect(ip, universe):
+    def connect(ip, pixel_count):
         """
         Create and return a controller ready for use
         """
+        if pixel_count > WledMaxPixelsExceeded.max_pixels:
+            raise WledMaxPixelsExceeded(pixel_count)
+
         wled = Wled()
         wled.ip = ip
-        wled.universe = universe
+        wled.pixel_count = pixel_count
+        wled.universes = math.ceil(pixel_count / 170)
         wled.reconnect()
 
         return wled
 
     def stop(self):
         """
-        Close the communication and destroy the sender (recreated on next connection)
+        Close the communication and destroy the sender (recreated on next
+        connection)
         """
         if self.sender:
-            self.sender.deactivate_output(1)
+            for output in self.sender.get_active_outputs():
+                self.sender.deactivate_output(output)
+
             self.sender.stop()
             self.sender = None
             
@@ -36,10 +47,18 @@ class Wled:
         """
         Recreate and set up the sender
         """
-        self.sender = sacn.sACNsender()
+        self.sender = sacn.sACNsender(
+            bind_port=source['bind_port'],
+            source_name=source['name'],
+            cid=source['cid'],
+            fps=source['fps']
+        )
         self.sender.start()
-        self.sender.activate_output(self.universe)
-        self.sender[self.universe].destination = self.ip
+
+        # Configure universes needed for all pixels (one per 170 pixels)
+        for universe in range(1, self.universes+1):
+            self.sender.activate_output(universe)
+            self.sender[universe].destination = self.ip
         
         self.is_connected = True
 
@@ -47,11 +66,24 @@ class Wled:
         """
         Send some colors down stream
         """
-        data = []
-        
-        for color in color_list:
-            data.append(math.floor(color.rgb[0] * 255))
-            data.append(math.floor(color.rgb[1] * 255))
-            data.append(math.floor(color.rgb[2] * 255))
+        self.sender.manual_flush = True
 
-        self.sender[1].dmx_data = tuple(data)
+        # Split data into universe chunks
+        chunks = [
+            color_list[ i*170: (i*170)+170 ] for i in range(self.universes)
+        ]
+        
+        universe = 1
+        for colors in chunks:
+            data = []
+            for color in colors:
+                data.append(math.floor(color.rgb[0] * 255))
+                data.append(math.floor(color.rgb[1] * 255))
+                data.append(math.floor(color.rgb[2] * 255))
+            
+            self.sender[universe].dmx_data = tuple(data)
+            universe += 1
+
+        # After all universe data is updated, send to device
+        self.sender.flush()
+        self.sender.manual_flush = False
