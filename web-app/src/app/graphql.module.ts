2ed1ec96873402c0d1987686f4b215bc9c5f906c
Kyler Freas <kmf5285@rit.edu>
2022-11-20 22:11:41 -0500

    add config for subscriptions

--- web-app/src/app/graphql.module.ts
+++ web-app/src/app/graphql.module.ts
@@ -1,16 +1,42 @@
 import { NgModule } from '@angular/core';
 import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
-import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
+import { ApolloClientOptions, InMemoryCache, split } from '@apollo/client/core';
+import { WebSocketLink } from '@apollo/client/link/ws'
+import { getMainDefinition } from '@apollo/client/utilities'
 import { HttpLink } from 'apollo-angular/http';
 
 import { APIHelper } from 'helpers/api-helper';
 
-const uri = `${APIHelper.getBaseUrl()}/graphql`;
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+  // Create an http link:
+  const http = httpLink.create({
+    uri: `${APIHelper.getBaseUrl()}/graphql`
+  })
+
+  // Create a WebSocket link:
+  const ws = new WebSocketLink({
+    uri: `${APIHelper.getBaseUrl('ws')}/graphql`,
+    options: {
+      reconnect: true
+    }
+  })
+
+  // using the ability to split links, you can send data to each link
+  // depending on what kind of operation is being sent
+  const link = split(
+    // split based on operation type
+    ({ query }) => {
+      const definition = getMainDefinition(query)
+      return definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
+    },
+    ws,
+    http
+  )
+
   return {
-    link: httpLink.create({uri}),
+    link,
     cache: new InMemoryCache(),
-  };
+  }
 }
 
 @NgModule({
