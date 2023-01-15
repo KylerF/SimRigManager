acf0e1bf2458c88883054da001e3b2cd532ccadd
Kyler Freas <kmf5285@rit.edu>
2023-01-15 11:16:14 -0500

    fix test

--- web-app/src/app/services/iracing-graphql.service.spec.ts
+++ web-app/src/app/services/iracing-graphql.service.spec.ts
@@ -1,16 +1,27 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
+import {
+  ApolloTestingModule,
+  ApolloTestingController,
+} from 'apollo-angular/testing';
 
-import { IracingGraphqlService } from './iracing-graphql.service';
+import { IracingDataGQLWeekendInfo } from './iracing-graphql.service';
 
-describe('IracingGraphqlService', () => {
-  let service: IracingGraphqlService;
+describe('IracingDataGQLWeekendInfo', () => {
+  let controller: ApolloTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(IracingGraphqlService);
+    TestBed.configureTestingModule({
+      imports: [ ApolloTestingModule ]
+    });
+
+    controller = TestBed.inject(ApolloTestingController);
   });
 
-  it('should be created', () => {
+  it('should be created', inject([IracingDataGQLWeekendInfo], (service: IracingDataGQLWeekendInfo) => {
     expect(service).toBeTruthy();
+  }));
+
+  afterEach(() => {
+    controller.verify();
   });
 });
