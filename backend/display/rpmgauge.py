4c6c35f66a4b6cd8994f78f70133a72e1f3c274e
KylerF <kmf5285@rit.edu>
2024-01-31 23:16:05 -0500

    change to single strip rpm gauge

--- backend/display/rpmgauge.py
+++ backend/display/rpmgauge.py
@@ -3,9 +3,8 @@ from colour import Color
 
 class RpmGauge:
     """
-    A dual LED rpm gauge - maps the car's RPM to a color gradient
+    An LED strip rpm gauge that maps the car's RPM to a color gradient
     """
-
     def __init__(self, led_count, color_theme, rpm=0, idle_rpm=0, redline=20000):
         self.led_count = led_count
         self.start_color = color_theme.primary_color
@@ -15,7 +14,9 @@ class RpmGauge:
         self.redline = redline
 
         self.full_gradient = list(
-            self.start_color.range_to(self.end_color, self.led_count // 2)
+            self.start_color.range_to(
+                self.end_color, self.led_count
+            )
         )
 
     def set_rpm(self, rpm):
@@ -52,14 +53,10 @@ class RpmGauge:
         if self.rpm == 0:
             return []
 
-        length = self.translate(self.rpm, 0, self.redline, 0, self.led_count // 2)
+        length = self.translate(self.rpm, 0, self.redline, 0, self.led_count)
         colors = self.full_gradient[0:length]
 
-        return (
-            colors
-            + [Color("black")] * (self.led_count // 2 - len(colors)) * 2
-            + colors[::-1]
-        )
+        return colors + [Color("black")] * (self.led_count - len(colors))
 
     def translate(self, value, left_min, left_max, right_min, right_max):
         """
